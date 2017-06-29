bd36ed43f6ec49899f060b231a0b01f18591ca19
alifensome <alifensome@hotmail.co.uk>
2017-06-29 17:56:04 +0100

    Committing file for a simple program in C#
    Program which writes strings backwards

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program to reverse a string and print it out
+            string OldString = "Zebra"; // string which will be reversed
+            string NewString = ""; // string to hold 
+            // loop over length of chars in string
+            for (int i = 0; i < OldString.Length; i++)
+            {
+                NewString += OldString[OldString.Length-i-1]; // starting at the last index add onto the NewString
+            }
+            System.Console.WriteLine(NewString); // output to concole
+            System.Console.Read(); // Read so as to not close concole
+
+        }
+    }
+}
